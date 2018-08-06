dfbe111fb603a889ebce90d7e22db1913f154c30
zw <15835206156@163.com>
2018-08-06 12:00:54 +0800

    修改了哈！

--- src/main/java/com/rtdl/App.java
+++ src/main/java/com/rtdl/App.java
@@ -6,6 +6,6 @@ package com.rtdl;
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "今天下雨了！" );
+        System.out.println( "今天下雨了！不要出去" );
     }
 }
